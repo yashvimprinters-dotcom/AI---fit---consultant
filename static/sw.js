54b5dde9fcf2837d60f2be8a8fa878b9d713f4ce
yashvimprinters-dotcom <yashvimprinters@gmail.com>
2026-09-19 12:57:13 +0530

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
